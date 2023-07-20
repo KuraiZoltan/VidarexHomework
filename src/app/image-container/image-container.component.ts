a1d4e49f82758995c2008ad875eec366c8599ac9
KuraiZoltan <kz1022@gmail.com>
2023-07-20 12:14:56 +0200

    New image zoom library update

--- src/app/image-container/image-container.component.ts
+++ src/app/image-container/image-container.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgxImgZoomService } from 'ngx-img-zoom';
 
 @Component({
   selector: 'app-image-container',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./image-container.component.css']
 })
 export class ImageContainerComponent {
+  myThumbnail = '../assets/18492-city-cityscape-metropolitan_area-capital_city-the_hague-7680x4320.jpg';
+  myFullresImage = '../assets/18492-city-cityscape-metropolitan_area-capital_city-the_hague-7680x4320.jpg';
+  constructor() { }
 
 }
