505e796809ec53e164953e769021369ffefc49ca
KuraiZoltan <kz1022@gmail.com>
2023-07-22 22:26:06 +0200

    Zooming, magnification changing, saturation changing implementation

--- src/app/image-container/image-container.component.ts
+++ src/app/image-container/image-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-image-container',
@@ -6,37 +6,60 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./image-container.component.css']
 })
 export class ImageContainerComponent {
-  @ViewChild('lensImage') lensImage!: ElementRef<HTMLImageElement>;
+  @ViewChild('photo') photo !: ElementRef<HTMLImageElement>;
+  @ViewChild('container') container!: ElementRef<HTMLDivElement>;
+  @ViewChild('lens') lens!: ElementRef<HTMLDivElement>;
+  @ViewChild('lensPhoto') lensPhoto!: ElementRef<HTMLImageElement>;
 
   magnification = 2;
   saturation = 100;
 
-  lensLeft = 0;
-  lensTop = 0;
-
-  imageLeft = 0;
-  imageTop = 0;
-
-  onMouseMove(e: MouseEvent) {
-    this.lensLeft = e.x;
-    this.lensTop = e.y;
-    const smallImage: HTMLImageElement = <HTMLImageElement> e.target
-    var img: HTMLImageElement = this.lensImage.nativeElement;
-    this.imageLeft = this.lensLeft / smallImage.clientWidth * img.naturalWidth;
-    this.imageTop = this.lensTop / smallImage.clientHeight * img.naturalHeight;
-    console.log(smallImage.clientWidth, smallImage.clientHeight)
-    /*console.log(this.imageLeft, this.imageTop)*/
+  onMouseOver(e: MouseEvent) {
+    this.lens.nativeElement.style.display = 'block';
   }
 
-  constructor() { }
+  onMouseOut(e: MouseEvent) {
+    this.lens.nativeElement.style.display = 'none';
+  }
+
+  onMousemove(e: MouseEvent) {
+    let lensBorderWidth = this.lens.nativeElement.offsetWidth - this.lens.nativeElement.clientWidth;
+    let lensBorderHeight = this.lens.nativeElement.offsetHeight - this.lens.nativeElement.clientHeight;
+
+    this.lens.nativeElement.style.left = e.x - this.lens.nativeElement.clientWidth - lensBorderWidth + 'px';
+    this.lens.nativeElement.style.top = e.y - this.lens.nativeElement.clientHeight - lensBorderHeight + 'px';
+
+    this.lensPhoto.nativeElement.style.width = this.photo.nativeElement.clientWidth * this.magnification + 'px'
+    this.lensPhoto.nativeElement.style.height = this.photo.nativeElement.clientHeight * this.magnification + 'px'
+
+    const widthRatio = this.lensPhoto.nativeElement.width / this.photo.nativeElement.clientWidth;
+    const heightRatio = this.lensPhoto.nativeElement.height / this.photo.nativeElement.clientHeight;
+
+    let realX = e.x * widthRatio - this.lens.nativeElement.clientWidth / 2;
+    let realY = e.y * heightRatio - this.lens.nativeElement.clientHeight / 2;
+
+    this.lensPhoto.nativeElement.style.translate = `-${realX}px -${realY}px`;
+  }
+
+  onWheelmove(event: any) {
+    if (event.deltaY === -100) {
+      if (this.magnification < 10) {
+        this.magnification += 1;
+      }
+    } else {
+      if (this.magnification > 1) {
+        this.magnification -= 1;
+      }
+    }
+  }
 
   changeMagnification(event: any) {
-    this.magnification = event.currentTarget.value
-    console.log(this.magnification)
+    this.magnification = event.currentTarget.value;
   }
 
   changeSaturation(event: any) {
-    this.saturation = event.currentTarget.value
-    console.log(this.saturation)
+    this.saturation = event.currentTarget.value;
   }
+
+  
 }
